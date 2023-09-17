e6d50e72c88d69985d04c54aa3907c3906dfb7c2
AMXELCbis <90291638+AMXELCbis@users.noreply.github.com>
2023-09-17 04:36:01 -0700

    add SC post

---
+++ Assets/SC Post Effects/Editor/Editors/KaleidoscopeEditor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEditor.Rendering;
+
+namespace SCPE
+{
+    [VolumeComponentEditor(typeof(Kaleidoscope))]
+    sealed class KaleidoscopeEditor : VolumeComponentEditor
+    {
+        SerializedDataParameter splits;
+
+        private bool isSetup;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            var o = new PropertyFetcher<Kaleidoscope>(serializedObject);
+            isSetup = AutoSetup.ValidEffectSetup<KaleidoscopeRenderer>();
+
+            splits = Unpack(o.Find(x => x.splits));
+        }
+
+
+        public override void OnInspectorGUI()
+        {
+            SCPE_GUI.DisplayDocumentationButton("kaleidoscope");
+
+            SCPE_GUI.DisplaySetupWarning<KaleidoscopeRenderer>(ref isSetup);
+
+            PropertyField(splits);
+        }
+    }
+}
---
+++ Assets/SC Post Effects/Runtime/Sharpen/Sharpen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+
+namespace SCPE
+{
+    [Serializable, VolumeComponentMenu("SC Post Effects/Image/Sharpen")]
+    public sealed class Sharpen : VolumeComponent, IPostProcessComponent
+    {
+        public ClampedFloatParameter amount = new ClampedFloatParameter(0f, 0f, 1f);
+        public ClampedFloatParameter radius = new ClampedFloatParameter(1f, 0.1f, 2f);
+
+        public bool IsActive() { return active && amount.value > 0f; }
+
+        public bool IsTileCompatible() => false;
+    }
+}
