d67efe0e9a4c3034f5e09586cd2555034119a099
AMXELCbis <90291638+AMXELCbis@users.noreply.github.com>
2023-10-04 15:17:59 -0700

    added com.unity.vectorgraphics, in order to use SVG images

---
+++ Assets/NumberAnimationSystem/Scripts/NumAnimData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NumAnimData", menuName = "Data Tools/Create Number Animation Data", order = 1)]
+public class NumAnimData : ScriptableObject
+{
+  public int _total;
+  public int _animationTime;
+}
---
+++ Assets/NumberAnimationSystem/Scripts/NumAnimTestUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NumAnimTestUI : MonoBehaviour
+{
+    public NumAnimController _jpController;
+    public GameObject _newJpNum;
+    public GameObject _newAnimationTime;
+    private bool _zeroStart;
+
+    public void Start()
+    {
+        GameObject.Find("StartFromZero").GetComponent<Toggle>().onValueChanged.AddListener(isOn => OnClick(isOn));
+    }
+
+    public void OnAddBtnDown()
+    {
+        if (_zeroStart)
+        {
+            _jpController.m_currentNum = 0;
+        }
+        
+        NumAnimData d = ScriptableObject.CreateInstance<NumAnimData>();
+        d._total = int.Parse(_newJpNum.GetComponent<Text>().text);
+        d._animationTime = int.Parse(_newAnimationTime.GetComponent<Text>().text);
+        _jpController.Animate(d);
+    }
+
+    public void OnClick(bool isOn)
+    {
+        if (isOn)
+        {
+            _zeroStart = true;
+        }
+        else
+        {
+            _zeroStart = false;
+        }
+    }
+
+}
