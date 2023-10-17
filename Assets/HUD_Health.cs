705ae2bb12bff891f07f88db9bb286756a496f9c
AMXELCbis <90291638+AMXELCbis@users.noreply.github.com>
2023-10-17 16:04:36 -0700

    basic health bar with color finished, about to code it

--- Assets/HUD_Health.cs
+++ Assets/HUD_Health.cs
@@ -1,3 +1,4 @@
+using RengeGames.HealthBars;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,15 @@ using UnityEngine;
 public class HUD_Health : MonoBehaviour
 {
 
+	[SerializeField] private RadialSegmentedHealthBar Healthbar;
 	[SerializeField] private GameObject player;
 
 	// Start is called before the first frame update
 	void Start()
     {
-        
-    }
+		//Healthbar.
+
+	}
 
     // Update is called once per frame
     void Update()
