e97e38194edfcfc0d1a5196b634cd5b9f8a02218
AMXELCbis <90291638+AMXELCbis@users.noreply.github.com>
2023-10-18 18:22:10 -0700

    polished Loudness bar

--- Assets/Script/UI/HUD_Loudness.cs
+++ Assets/Script/UI/HUD_Loudness.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Michsky.MUIP;
+using System;
+using Unity.Mathematics;
+using DG.Tweening;
+using TreeEditor;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
 
 public class HUD_Loudness : MonoBehaviour
 {
@@ -12,15 +19,22 @@ public class HUD_Loudness : MonoBehaviour
 	public CameraController cameraController;
 	public UnityEngine.UI.Image Background;
 	public UnityEngine.UI.Image Bar;
-	public TextMeshProUGUI Text;
+	public SpriteRenderer Speaker;
+	public GameObject ObjectSpeaker;
 	[SerializeField] private Michsky.MUIP.ProgressBar myBar;
 	[SerializeField] private Rigidbody player;
 	[SerializeField] private PlayerController Controller;
-	
+	[SerializeField] private List<Sprite> Speakersprite;
+	[SerializeField] public Volume m_Volume;
+
 
 	private float A_Background;
-	private float A_Text;
+	private float A_Speaker;
 	private float A_InitBar;
+
+	MotionBlur MoBCompent;
+
+	private Vector3 P_Speaker;
 
 	private float A_Speed = 0.05f;
 	private float A_FastSpeed = 0.7f;
@@ -30,9 +44,16 @@ public class HUD_Loudness : MonoBehaviour
 	void Start()
 	{
 		MaxSpeed = Controller.maxSpeed;
-		A_Text = 0.7f;
+		A_Speaker = 0.8f;
 		A_Background = (float)15 / 255;
 		A_InitBar = (float)132 / 255;
+
+		P_Speaker = Speaker.transform.position;
+
+
+		m_Volume.profile.TryGet<MotionBlur>(out MoBCompent);
+
+		//MotionBlur_Volume = Volume
 	}
 
 	void checkA()
@@ -42,7 +63,7 @@ public class HUD_Loudness : MonoBehaviour
 			Vector4 temp = Background.color;
 			temp.w = 0;
 			Background.color = Vector4.Lerp(Background.color, temp, A_Speed);
-			Text.color = Vector4.Lerp(Text.color, temp, A_Speed);
+			Speaker.color = Vector4.Lerp(Speaker.color, temp, A_Speed);
 			temp = Bar.color;
 			temp.w = 0;
 			Bar.color = Vector4.Lerp(Bar.color, temp, A_Speed);
@@ -52,8 +73,8 @@ public class HUD_Loudness : MonoBehaviour
 			Vector4 temp = Background.color;
 			temp.w = A_Background;
 			Background.color = Vector4.Lerp(Background.color, temp, A_Speed);
-			temp.w = A_Text;
-			Text.color = Vector4.Lerp(Text.color, temp, A_Speed);
+			temp.w = A_Speaker;
+			Speaker.color = Vector4.Lerp(Speaker.color, temp, A_Speed);
 			temp = Bar.color;
 			temp.w = A_InitBar;
 			Bar.color = Vector4.Lerp(Bar.color, temp, A_FastSpeed);
@@ -63,6 +84,35 @@ public class HUD_Loudness : MonoBehaviour
 	}
 
 
+	void checkSpeaker()
+	{
+		if (myBar.currentPercent < 10)
+		{
+			Speaker.sprite = Speakersprite[0];
+		}
+		else if (myBar.currentPercent < 45)
+		{
+			Speaker.sprite = Speakersprite[1];
+		}
+		else if (myBar.currentPercent < 75)
+		{
+			Speaker.sprite = Speakersprite[2];
+		}
+		else if (myBar.currentPercent < 100)
+		{
+			Speaker.sprite = Speakersprite[3];
+		}
+
+	}
+
+	void checkMontionBlur()
+	{
+		MoBCompent.intensity.value = myBar.currentPercent / 100f;
+
+		if (MoBCompent.intensity.value < 0.2f)
+			MoBCompent.intensity.value = 0.2f;
+	}
+
 	void Update()
 	{
 		checkA();
@@ -70,6 +120,18 @@ public class HUD_Loudness : MonoBehaviour
 		CurrentSpeed = player.velocity.magnitude;
 
 		myBar.currentPercent = CurrentSpeed / MaxSpeed * 100;
+
+		checkSpeaker();
+
+		if(cameraController.isUpPoint)
+		{
+			checkMontionBlur();
+		}
+		else
+		{
+			if(MoBCompent.intensity.value!= 0.2f)
+				MoBCompent.intensity.value = 0.2f;
+		}	
 
 		//P is 0 ~ 100
 		float P = myBar.currentPercent;
@@ -83,6 +145,7 @@ public class HUD_Loudness : MonoBehaviour
 
 			Vector4 temp = Bar.color;
 			temp.x = FirstP;
+			temp.y = 1f;
 			Bar.color = temp;
 
 		}
@@ -97,6 +160,8 @@ public class HUD_Loudness : MonoBehaviour
 			//Bar.color = Vector4.Lerp(Bar.color, temp, A_FastSpeed);
 
 		}
+
+
 
 
 
