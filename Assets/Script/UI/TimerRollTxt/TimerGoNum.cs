e934ee042a80320e753791668e20f6e050ce7ab4
sousya <412172127@qq.com>
2023-10-23 15:02:19 +0800

    滚动数字 2.0
    主要脚本 TimerText
    beginPos 控制字符结束位置（控制y轴用）
    totalTime 控制滚动总时长（useTotalTime == true时生效）
    rollTime 每格滚动一次时间（useTotalTime == false时生效）
    
    使用方法 设定初始值 SetBeginNum(初始数值) 然后StartRoll(目标数值)

--- Assets/Script/UI/TimerRollTxt/TimerGoNum.cs
+++ Assets/Script/UI/TimerRollTxt/TimerGoNum.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -9,18 +9,64 @@ using DG.Tweening;
 public class TimerGoNum : MonoBehaviour
 {
 	[SerializeField]
-	TextMeshProUGUI num1, num2, num3;
+	TextMeshProUGUI num1, num2, num3, curTextNum, otherTextNum;
+
+	TextMeshProUGUI waitTextNum
+	{
+		get
+		{
+			if (curTextNum == num1)
+			{
+				if(otherTextNum == num2)
+				{
+					return num3;
+				}
+				else
+				{
+					return num2;
+				}
+			}
+			else if(curTextNum == num2)
+			{
+				if(otherTextNum == num3)
+				{
+					return num1;
+				}
+				else
+				{
+					return num3;
+				}
+			}
+			else if (curTextNum == num3)
+			{
+				if(otherTextNum == num1)
+				{
+					return num2;
+				}
+				else
+				{
+					return num1;
+				}
+			}
+
+			return null;
+		}
+	}
 
 	public int curText = 1;
-	public int curNum;
+	public int curNum = -1;
 	public int roundNum = 3;
 	public int lastNum = 3;
 	public float slowDownScale = 1.1f;
 	public float lastWaitTime = 1f;
 
 	public bool isUp = true;
+	bool lastUp;
+
 
 	Vector3 showPos, upPos, downPos;
+
+	public delegate void ShowNextNum(int num);
 
 	private void Awake()
 	{
@@ -29,43 +75,188 @@ public class TimerGoNum : MonoBehaviour
 		downPos = num3.transform.localPosition;
 	}
 
-	public void SetBeginNum(int num, int defLastNum, int defRoundNum)
+	public void SetBeginNum(int num)
+	{
+		curNum = num;
+
+		num1.text = num.ToString();
+		num2.text = calcNextPlus().ToString();
+		num3.text = calcNextMinues().ToString();
+		curTextNum = num1;
+		otherTextNum = num2;
+		isUp = true;
+		lastUp = true;
+	}
+
+	public void SetNum()
 	{
 		int nextNum = 0;
-		curNum = num;
-		if (num == 9)
+
+		if(isUp)
+		{
+			nextNum = calcNextPlus();
+			//if (curNum % 2 == 1)
+			//{
+			//	num2.text = curNum.ToString();
+			//	num1.text = nextNum.ToString();
+			//}
+			//else
+			//{
+			//	num1.text = curNum.ToString();
+			//	num2.text = nextNum.ToString();
+			//}
+			otherTextNum.text = nextNum.ToString();
+			curTextNum.text = curNum.ToString();
+			waitTextNum.text = calcNextMinues().ToString();
+		}
+		else
+		{
+			nextNum = calcNextMinues();
+			//if (curNum % 2 == 1)
+			//{
+			//	num3.text = curNum.ToString();
+			//	num1.text = nextNum.ToString();
+			//}
+			//else
+			//{
+			//	num1.text = curNum.ToString();
+			//	num3.text = nextNum.ToString();
+			//}
+
+			otherTextNum.text = nextNum.ToString();
+			curTextNum.text = curNum.ToString();
+			waitTextNum.text = calcNextPlus().ToString();
+		}
+		
+
+	}
+
+	public int calcNextPlus()
+	{
+		int nextNum = 0;
+		if (curNum == 9)
 		{
 			nextNum = 0;
 		}
 		else
 		{
-			nextNum = num + 1;
+			nextNum = curNum + 1;
 		}
-
-		num1.text = num.ToString();
-		num2.text = nextNum.ToString();
-		lastNum = defLastNum;
-		roundNum = defRoundNum;
+		return nextNum;
 	}
 
+	public int calcNextMinues()
+	{
+		int nextNum = 0;
+		if (curNum == 0)
+		{
+			nextNum = 9;
+		}
+		else
+		{
+			nextNum = curNum - 1;
+		}
+		return nextNum;
+	}
 	public void ChangeNum(float time)
 	{
-		num1.transform.DOLocalMove(num1.transform.localPosition + new Vector3(0, num1.rectTransform.rect.height), time);
-		Tweener tweener = num2.transform.DOLocalMove(num2.transform.localPosition + new Vector3(0, num2.rectTransform.rect.height), time);
-
-		tweener.onComplete = () =>
+		if(isUp == true)
 		{
-			if (curNum % 2 == 1)
-			{
-				num1.transform.localPosition = upPos;
-			}
-			else if (curNum % 2 == 0)
-			{
-				num2.transform.localPosition = upPos;
-			}
-			//SetNum();
+			curTextNum.transform.DOLocalMove(curTextNum.transform.localPosition + new Vector3(0, curTextNum.rectTransform.rect.height), time);
+			Tweener tweener = otherTextNum.transform.DOLocalMove(otherTextNum.transform.localPosition + new Vector3(0, otherTextNum.rectTransform.rect.height), time);
 
-		};
+			tweener.onComplete = () =>
+			{
+				TextMeshProUGUI temp = curTextNum;
+				curTextNum = otherTextNum;
+				otherTextNum = temp;
+				otherTextNum.transform.localPosition = upPos;
+				SetNum();
 
+			};
+		}
+		else
+		{
+			curTextNum.transform.DOLocalMove(curTextNum.transform.localPosition - new Vector3(0, curTextNum.rectTransform.rect.height), time);
+			Tweener tweener = otherTextNum.transform.DOLocalMove(otherTextNum.transform.localPosition - new Vector3(0, otherTextNum.rectTransform.rect.height), time);
+
+			tweener.onComplete = () =>
+			{
+				TextMeshProUGUI temp = curTextNum;
+				curTextNum = otherTextNum;
+				otherTextNum = temp;
+				otherTextNum.transform.localPosition = downPos;
+
+				SetNum();
+
+			};
+		}
+		
+
+	}
+
+	public void test(int nextNum)
+	{
+		//BeginChange(nextNum, false);
+	}
+
+	public void BeginChange(int nextNum, float time, bool up = true)
+	{
+		if (nextNum == curNum)
+		{
+			return;
+		}
+		if(curNum == -1)
+		{
+			num1.text = "";
+			num2.text = nextNum.ToString();
+			num3.text = nextNum.ToString();
+			curTextNum = num1;
+			isUp = up;
+			lastUp = up;
+			if(isUp)
+			{
+				otherTextNum = num2;
+			}
+			else
+			{
+				otherTextNum = num3;
+			}
+		}
+		else
+		{
+			if (curNum > nextNum)
+			{
+				if (curNum == 9 && nextNum == 0)
+				{
+					isUp = true;
+				}
+				else
+				{
+					isUp = false;
+				}
+			}
+			else
+			{
+				if (curNum == 0 && nextNum == 9)
+				{
+					isUp = false;
+				}
+				else
+				{
+					isUp = true;
+				}
+			}
+		}
+		if(lastUp != isUp)
+		{
+			otherTextNum = waitTextNum;
+			lastUp = isUp;
+		}
+
+		curNum = nextNum;
+
+
+		ChangeNum(time);
 	}
 }
